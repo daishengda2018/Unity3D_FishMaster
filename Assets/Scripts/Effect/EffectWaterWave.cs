2bed62fc6c9e669bd70f3e410f0ac853a4488dd1
代圣达 <dsd13502@gmail.com>
2022-04-02 20:12:30 +0800

    init

---
+++ Assets/Scripts/Effect/EffectWaterWave.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectWaterWave : MonoBehaviour
+{
+    public Texture[] textures;
+    private Material _material;
+    private int index;
+
+    private void Start()
+    {
+        _material = GetComponent<MeshRenderer>().material;
+        InvokeRepeating(nameof(ChangeTexture), 0f, 0.1f);
+    }
+
+    private void ChangeTexture()
+    {
+        _material.mainTexture = textures[index++];
+        index %= textures.Length;
+    }
+}
